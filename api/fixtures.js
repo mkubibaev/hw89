c97a297375b7dda5a458f7844c0ed36b08cbf353
mkubibaev <mkubibaev@gmail.com>
2019-05-24 10:14:18 +0600

    changed test fb user passwords

--- api/fixtures.js
+++ api/fixtures.js
@@ -17,8 +17,8 @@ const run = async () => {
     }
 
     const users = await User.create(
-        {username: 'calopdumru_1558613014@tfbnw.net', displayName: 'William Alcgbjedceifa Shepardson', password: '123', role: 'user', token: 'zxcvb'},
-        {username: 'nifbxffjnz_1558613008@tfbnw.net', displayName: 'Margaret Alcgajcjhbbba Fergiesen', password: '123', role: 'admin', token: 'qwerty'},
+        {username: 'calopdumru_1558613014@tfbnw.net', displayName: 'William Alcgbjedceifa Shepardson', password: 'qaz2wsx', role: 'user', token: 'zxcvb'},
+        {username: 'nifbxffjnz_1558613008@tfbnw.net', displayName: 'Margaret Alcgajcjhbbba Fergiesen', password: 'qaz2wsx', role: 'admin', token: 'qwerty'},
         {username: 'admin', displayName: 'Administrator', password: '123', role: 'admin', token: 'asdfg'}
     );
 
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-username: 'calopdumru_1558613014@tfbnw.net', displayName: 'William Alcgbjedceifa Shepardson', password: '123', role: 'user'
+username: 'calopdumru_1558613014@tfbnw.net', displayName: 'William Alcgbjedceifa Shepardson', password: 'qaz2wsx', role: 'user'
 
-username: 'nifbxffjnz_1558613008@tfbnw.net', displayName: 'Margaret Alcgajcjhbbba Fergiesen', password: '123', role: 'admin'
+username: 'nifbxffjnz_1558613008@tfbnw.net', displayName: 'Margaret Alcgajcjhbbba Fergiesen', password: 'qaz2wsx', role: 'admin'
